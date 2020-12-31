2ac6212ff41d0be03468aad8e54010bafe10a2fe
wndnjs9878 <48276633+wndnjs9878@users.noreply.github.com>
2020-12-31 15:55:07 +0900

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by 이주원 on 2020/12/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
